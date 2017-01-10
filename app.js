9a98d03d92caa3e92820677ce25c17503ece32b4
Vaibhav <vaibhav@cantern.in>
2017-01-10 21:30:06 +0530

    initial commit with app skeleton and running server

---
+++ app.js
@@ -0,0 +1,23 @@
+//Entry point of the Server
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// loading routes
+var routes = require('./api/routes');
+
+// Define the port to run on
+app.set('port', 3000);
+
+// Enable parsing for forms submisstions
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//creating server to listen to request
+
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Library management running on port ' + port);
+});
